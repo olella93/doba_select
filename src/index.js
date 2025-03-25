e889cb6ed865230dfd84f351d0ad219aa94e7747
Richard <olella93@gmail.com>
2025-03-25 13:40:25 +0300

    function to display recommendation added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // function to fetch artists based on genre input
 
 function getRecommendations() {
-    let genre = document.getElementById("genreInput").ariaValueMax.trim();
+    let genre = document.getElementById("genreInput").value.trim();
 
     //checking if input is empty
     if (genre === ""){
@@ -10,7 +10,7 @@ function getRecommendations() {
     }
 
     //fetch artists related to the genre from Deezer API
-    fetch("https://api.deezer.com/search/artist?q=${genre}")
+    fetch(`https://api.deezer.com/search/artist?q=${genre}`)
     .then(response => response.json ())
     .then(data => {
         displayRecommendations(data.data);
@@ -19,6 +19,17 @@ function getRecommendations() {
         console.error("Error fetching data:", error);
         alert("Something went wrong. Please try again!");
     });
+}
+
+//function to display artist recommendation on page
+function displayRecommendations(artists) {
+    const recommendationsDiv = document.getElementById("recommendations");
+    recommendationsDiv.innerHTML = ""; //clears previous results
+
+    // Loop through the artists to create elements for them
+    artists.forEach(artist => {
+        const
+    })
 }
 
 
